cbe6470432d23e5bcc261f2112f74a7511ea70dc
Robson Silva <robsonveloso@hotmail.com>
2023-01-23 19:56:56 -0300

    Commit 3 - inserindo enor nota nova brantch

--- src/diarionota/DiarioNota.java
+++ src/diarionota/DiarioNota.java
@@ -15,7 +15,8 @@ public class DiarioNota {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Diario cursoJava = new Diario();
+    
   }
   
 }

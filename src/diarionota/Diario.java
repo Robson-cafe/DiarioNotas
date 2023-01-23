966f6d6fd48d228dc558a68e86442d7512478682
Robson Silva <robsonveloso@hotmail.com>
2023-01-23 19:28:26 -0300

    Commit 1

---
+++ src/diarionota/Diario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diarionota;
+
+/**
+ *
+ * @author Robson
+ */
+public class Diario {
+  private String nomeCurso;
+  double[] notasAlunos;
+  
+  public String getNomeCurso() {
+          return nomeCurso;
+  }
+  
+  public void setNomeCurso(String _nomeCurso) {
+          nomeCurso = _nomeCurso;
+  }
+}
+
+
